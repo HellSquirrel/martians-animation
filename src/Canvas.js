1f4263442554f52c54e1cabaf159e2da909cef7d
HellSquirrel <zloymult@gmail.com>
2019-04-01 19:36:49 +0300

    Add dl face recognition

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,14 +2,66 @@ import React, { useRef } from "react";
 import classNames from "classnames/bind";
 import cs from "./Canvas.module.css";
 import martians from "./animated/martians.svg";
+import * as bodyPix from "@tensorflow-models/body-pix";
 
 const cx = classNames.bind(cs);
 
 const width = 266 * 2;
 const height = 190 * 2;
+const outputStride = 8;
+const segmentationThreshold = 0.5;
+
+const toMask = (partSegmentation, originalImageData) => {
+  const { width, height, data } = partSegmentation;
+  console.log(width, height);
+  let { x0, y0, x1, y1 } = { x0: 0, y0: 0, x1: width, y1: height };
+  const originalBytes = originalImageData.data;
+  const bytes = new Uint8ClampedArray(width * height * 4);
+
+  for (let i = 0; i < height * width; ++i) {
+    const partId = Math.round(data[i]);
+    const j = i * 4;
+
+    if (partId === 0 || partId === 1) {
+      const x = i % width;
+      const y = Math.ceil(i / width);
+
+      if (x > x0) {
+        x0 = x;
+      }
+
+      if (y > y0) {
+        y0 = y;
+      }
+
+      if (x < x1) {
+        x1 = x;
+      }
+
+      if (y < y1) {
+        y1 = y;
+      }
+
+      bytes[j + 0] = originalBytes[j + 0];
+      bytes[j + 1] = originalBytes[j + 1];
+      bytes[j + 2] = originalBytes[j + 2];
+      bytes[j + 3] = 255;
+    } else {
+      bytes[j + 0] = 0;
+      bytes[j + 1] = 0;
+      bytes[j + 2] = 0;
+      bytes[j + 3] = 0;
+    }
+  }
+
+  return {
+    image: new ImageData(bytes, width, height),
+    dims: { x: x0, y: y0, width: x1 - x0, height: y1 - y0 }
+  };
+};
 
 const getUserMedia = async (canvas, video) => {
-  console.log(canvas);
+  const net = await bodyPix.load();
   let stream = null;
 
   try {
@@ -21,22 +73,45 @@ const getUserMedia = async (canvas, video) => {
       }
     });
     video.srcObject = stream;
-    video.onloadedmetadata = () => {
+    video.onloadedmetadata = async () => {
       video.play();
       const ctx = canvas.getContext("2d");
       requestAnimationFrame(() => {
-        //ctx.clearRect(0, 0, width, height);
+        ctx.clearRect(0, 0, width, height);
         const img = new Image();
         img.src = martians;
         img.onload = () =>
-          requestAnimationFrame(() => {
+          requestAnimationFrame(async () => {
             ctx.drawImage(img, 0, 0, width, height);
-            ctx.drawImage(
+
+            const personSegmentation = await net.estimatePartSegmentation(
               video,
+              outputStride,
+              segmentationThreshold
+            );
+
+            const offscreenCanvas = document.createElement("canvas");
+            offscreenCanvas.width = width;
+            offscreenCanvas.height = height;
+            const offscreenContext = offscreenCanvas.getContext("2d");
+            offscreenContext.drawImage(video, 0, 0);
+            const masked = toMask(
+              personSegmentation,
+              offscreenContext.getImageData(0, 0, width, height)
+            );
+            offscreenContext.putImageData(masked.image, 0, 0);
+
+            console.log(masked.dims);
+            ctx.drawImage(
+              offscreenCanvas,
+              masked.dims.x,
+              masked.dims.y,
+              masked.dims.width,
+              masked.dims.height,
               width * 0.594,
               height * 0.04,
               width * 0.296,
-              height * 0.381
+              height * 0.4
             );
           });
       });
@@ -50,10 +125,10 @@ const loadPhoto = (canvas, link) => {
   try {
     canvas.toBlob(blob => {
       link.href = URL.createObjectURL(blob);
-      link.download = "photo.jpg";
+      link.download = "photo.png";
       link.click();
       URL.revokeObjectURL(blob);
-    }, "image/jpeg");
+    }, "image/png");
   } catch (e) {
     console.log("error while loading data", e);
   }
@@ -80,7 +155,7 @@ const Canvas = () => {
         height={height}
       />
       <button onClick={() => getUserMedia(canvas.current, video.current)}>
-        Make photo
+        Take a photo
       </button>
       <button onClick={() => loadPhoto(canvas.current, link.current)}>
         Load photo
