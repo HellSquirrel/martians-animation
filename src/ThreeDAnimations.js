ed71ac600cddae3121e4014101f2675e05e7dfc7
HellSquirrel <zloymult@gmail.com>
2019-04-01 21:42:41 +0300

    add cube transformation

--- src/ThreeDAnimations.js
+++ src/ThreeDAnimations.js
@@ -6,22 +6,34 @@ const cx = classNames.bind(cs);
 
 const ThreeDAnimations = () => {
   const [animation, setAnimation] = useState(null);
-  const [perspective, setPerspective] = useState(0);
-  console.log(animation, cs);
+  const [perspective, setPerspective] = useState(1000);
+  const [perspectiveOrigin, setPerspectiveOrigin] = useState('center center');
   return (
     <div className={cx("threeDAnimations")}>
       <button onClick={() => setAnimation("rx")}>Rotate X</button>
       <button onClick={() => setAnimation("ry")}>Rotate Y</button>
       <button onClick={() => setAnimation("rz")}>Rotate Z</button>
       <button onClick={() => setAnimation("tz")}>Translate Z</button>
+      <button onClick={() => setAnimation("cube")}>Cube</button>
       <input
         type="number"
         value={perspective}
         onChange={({ currentTarget }) => setPerspective(currentTarget.value)}
       />
-      <div className="threeDContainer" style={{ perspective: `${perspective}px` }}>
+      <input
+        type="text"
+        value={perspectiveOrigin}
+        onChange={({ currentTarget }) => setPerspectiveOrigin(currentTarget.value)}
+      />
+      <div
+        className="threeDContainer"
+        style={{
+          perspective: `${perspective}px`,
+          perspectiveOrigin: `${perspectiveOrigin}`
+        }}
+      >
         {[...Array(6)].map((_, id) => (
-          <div key={id} className={cx("face", `face${1}`, animation)} />
+          <div key={id} className={cx("face", `face${id}`, animation)} />
         ))}
       </div>
     </div>
