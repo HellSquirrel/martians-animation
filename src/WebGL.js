2a9a5a6066d3bc6ba5c83ecce12d8e2dc1328085
HellSquirrel <zloymult@gmail.com>
2019-04-06 14:48:40 +0300

    add random updates

--- src/WebGL.js
+++ src/WebGL.js
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import math from "mathjs";
 
 const THREE = require("three");
 
@@ -18,7 +19,7 @@ require("three/examples/js/controls/OrbitControls");
 const width = 400;
 const height = 400;
 
-const init = canvas => {
+const init = (canvas, shouldUpdate) => {
   const scene = new Scene();
   const camera = new PerspectiveCamera(45, 1, 1, 500);
 
@@ -29,16 +30,38 @@ const init = canvas => {
 
   renderer.setSize(width, height);
   renderer.setClearColor("#e0e0e0");
+  const light = new DirectionalLight("fff", 0.5);
   scene.add(new AmbientLight("#fff", 0.5));
-  scene.add(new DirectionalLight("fff", 0.5));
+  scene.add(light);
 
   const loader = new THREE.ColladaLoader();
   loader.load("/exploded.dae", function(collada) {
     scene.add(collada.scene);
-    camera.position.z = 25;
+    let geometry = null;
 
+    collada.scene.traverse(el => {
+      if (el.isMesh) {
+        geometry = el.geometry;
+      }
+    });
+
+    camera.position.z = 20;
+    collada.scene.rotation.x = (-1 * Math.PI) / 4;
     const animate = () => {
       requestAnimationFrame(animate);
+
+      if (shouldUpdate) {
+        const positions = geometry.attributes.position.array;
+        const step = 0.0001;
+        console.log(geometry.attributes.position.array[0]);
+        for (let i = 40000; i < 45000; i++) {
+          positions[i] += math.random(-1 * step, step);
+          positions[i + 1] += math.random(-1 * step, step);
+          positions[i + 2] += math.random(-1 * step, step);
+        }
+        geometry.attributes.position.needsUpdate = true;
+      }
+
       renderer.render(scene, camera);
     };
     animate();
@@ -53,6 +76,9 @@ const WebGL = () => {
   return (
     <React.Fragment>
       <button onClick={() => init(canvas.current)}>GO!</button>
+      <button onClick={() => init(canvas.current, true)}>
+        GO with updates!
+      </button>
       <canvas width={width} height={height} ref={canvas} />
     </React.Fragment>
   );
