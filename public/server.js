bbd4565ddd8d954412c5ce3a8eed2e1cbf474ec0
HellSquirrel <zloymult@gmail.com>
2019-04-17 02:01:11 +0300

    Fix heroku deploy

--- public/server.js
+++ public/server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const path = require("path");
 const server = express();
-server.use("/", express.static(__dirname + "/build"));
+
+server.use("/", express.static(__dirname));
 server.listen(process.env.PORT);
+
+server.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "index.html"))
+);
